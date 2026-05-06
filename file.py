3f938af54d7d4b74defffcff595da7358e8bc540
smritid1 <78858325+smritid1@users.noreply.github.com>
2026-05-06 12:27:19 +0545

    Refactor validation functions for name and email

--- file.py
+++ file.py
@@ -1,16 +1,3 @@
-'''Project Instructions
-Implement a function called validate_name(), that takes in one parameter, which will be a datatype of string to check the user has inputted a valid name.
-
-The function should check that the user has inputted a name that is a data type of string.
-The function should check that the name is greater than two characters long.
-The function should return a boolean value, True if the name is valid, and False if not.
-Implement a function called validate_email(), that takes in one parameter, which will be a datatype of string to check the user has inputted a valid email.
-
-The function should check that the user has inputted an email that contains an '@' symbol.
-The function should check that the email contains any top-level domain included in the top_level_domains list variable. This has been preloaded to the workbook for you.
-The function should return a boolean value,True if the name is valid, and False if not.'''
-# Re-run this cell
-# Preloaded data for validating email domain.
 top_level_domains = [
     ".org",
     ".net",
